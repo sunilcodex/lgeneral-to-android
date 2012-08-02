6387ad740fe0cf62e1bcf514dab97c6772d4f96e
r.ramosfg <r.ramosfg@gmail.com>
2012-08-02 16:33:02 +0000

    Hace que nuestro Prefabs Hexagon este en el editor y se pueda ver directamente en la escena sin dar al play, con lo que muestre como es nuestro hexagono

---
+++ branches/Editor/HexagonEditor.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor (typeof (Hexagon))] 
+public class HexagonEditor : Editor {
+	[MenuItem ("GameObject/Create Other/Hexagon")]
+	static void Create(){
+		GameObject gameObject = new GameObject("Hexagon");
+		Hexagon s = gameObject.AddComponent<Hexagon>();
+		MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+		meshFilter.mesh = new Mesh();
+		s.Awake();
+	}
+}
