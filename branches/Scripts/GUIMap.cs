7874049d9fa4108b8186c815d3f2156bf6a8b45d
r.ramosfg@gmail.com <r.ramosfg@gmail.com@7aea5e75-5191-deb6-7d89-6dce49e5dd89>
2012-09-07 10:01:48 +0000

    Se ha procedido a añadir el espacio de nombres como posteriormente hemos dicho en este caso para estas clases irán dentro del nombre Engine

--- branches/Scripts/GUIMap.cs
+++ branches/Scripts/GUIMap.cs
@@ -2,31 +2,36 @@ using UnityEngine;
 using System.Collections;
 using System;
 using System.Xml;
+using System.IO;
+using System.Xml.Serialization;
+using Engine;
 
-public class GUIMap : MonoBehaviour
+namespace UI
 {
-	
-	public int width;
-	public int height;
-	public GameObject hexPrefab;
-	//public Material defaultTexture;
-	public string mapname;
-	//private string title;
-	//private string description;
-	
-	//public TipoSuelo[,] mapa;
-	
-	private void ReadXMLMapFile (string name)
+	public class GUIMap : MonoBehaviour
 	{
-		XmlDocument xDoc = new XmlDocument ();
-		xDoc.Load ("Assets/Maps/" + name + ".xml");
-		//title = xDoc.GetElementsByTagName("title")[0].InnerText;
-		//print (title);
-		//description = xDoc.GetElementsByTagName("description")[0].InnerText;
-		//print (description);
-		width = int.Parse (xDoc.GetElementsByTagName ("map_w") [0].InnerText);
-		height = int.Parse (xDoc.GetElementsByTagName ("map_h") [0].InnerText);
-		/*XmlNodeList terrain = xDoc.GetElementsByTagName("terrain");
+	
+		public int width;
+		public int height;
+		public GameObject hexPrefab;
+		//public Material defaultTexture;
+		public string mapname;
+		//private string title;
+		//private string description;
+	
+		//public TipoSuelo[,] mapa;
+	
+		private void ReadXMLMapFile (string name)
+		{
+			XmlDocument xDoc = new XmlDocument ();
+			xDoc.Load ("Assets/Maps/" + name + ".xml");
+			//title = xDoc.GetElementsByTagName("title")[0].InnerText;
+			//print (title);
+			//description = xDoc.GetElementsByTagName("description")[0].InnerText;
+			//print (description);
+			width = int.Parse (xDoc.GetElementsByTagName ("map_w") [0].InnerText);
+			height = int.Parse (xDoc.GetElementsByTagName ("map_h") [0].InnerText);
+			/*XmlNodeList terrain = xDoc.GetElementsByTagName("terrain");
 		XmlNodeList cells = ((XmlElement)terrain[0]).GetElementsByTagName("cell"); 
 		foreach (XmlElement node in cells){
 			int y = Convert.ToInt32(node.SelectSingleNode("position_h").InnerText);
@@ -38,60 +43,57 @@ public class GUIMap : MonoBehaviour
 		}
 		*/
 		
-		XmlNodeList terrain = xDoc.GetElementsByTagName("map");
-		foreach (XmlElement node in terrain){
-			int y = Convert.ToInt32(node.SelectSingleNode("height").InnerText);
-			int x = Convert.ToInt32(node.SelectSingleNode("width").InnerText);
-			string terrain_type = node.SelectSingleNode("celltype").InnerText.ToLower();
-			int orientation = Convert.ToInt32(node.SelectSingleNode("orientation").InnerText);
-			MakeMap(x,y,terrain_type,orientation);
+			XmlNodeList terrain = xDoc.GetElementsByTagName ("map");
+			foreach (XmlElement node in terrain) {
+				int y = Convert.ToInt32 (node.SelectSingleNode ("height").InnerText);
+				int x = Convert.ToInt32 (node.SelectSingleNode ("width").InnerText);
+				string terrain_type = node.SelectSingleNode ("celltype").InnerText.ToLower ();
+				int orientation = Convert.ToInt32 (node.SelectSingleNode ("orientation").InnerText);
+				MakeMap (x, y, terrain_type, orientation);
 			
-		}
+			}
 		
-	}
+		}
 	
-	
-	
-	private void MakeMap(int x, int y,string terrain_type,int orientation){
-		GameObject hex;
-		if (IsEven(x)){
-			hex = (GameObject)Instantiate(hexPrefab,new Vector3(x*1.5f,0,-Mathf.Sqrt(3)*y),Quaternion.identity);
+		private void MakeMap (int x, int y, string terrain_type, int orientation)
+		{
+			GameObject hex;
+			if (IsEven (x)) {
+				hex = (GameObject)Instantiate (hexPrefab, new Vector3 (x * 1.5f, 0, -Mathf.Sqrt (3) * y), Quaternion.identity);
 					
-		}
-		else{
-			hex = (GameObject)Instantiate(hexPrefab,new Vector3(x*1.5f,0,
-				-(Mathf.Sqrt(3)*y)-(Mathf.Sqrt(3)/2)),Quaternion.identity);
-		}
-		hex.transform.parent = this.gameObject.transform;
-		Hexagon hexScript = hex.GetComponent(typeof(Hexagon)) as  Hexagon;
-		print ("W:"+x+" H:"+y+" T:"+terrain_type+" O:"+orientation);
-		hexScript.actualTexture = orientation;
-		hexScript.maxTextures = TextureTable.GetMaxTextureOf(terrain_type);
-		if (hexScript.maxTextures==-1){
-			Debug.Log("Texture don't exit");
-		}
+			} else {
+				hex = (GameObject)Instantiate (hexPrefab, new Vector3 (x * 1.5f, 0,
+				-(Mathf.Sqrt (3) * y) - (Mathf.Sqrt (3) / 2)), Quaternion.identity);
+			}
+			hex.transform.parent = this.gameObject.transform;
+			Hexagon hexScript = hex.GetComponent (typeof(Hexagon)) as  Hexagon;
+			hexScript.actualTexture = orientation;
+			hexScript.maxTextures = TextureTable.GetMaxTextureOf (terrain_type);
+			if (hexScript.maxTextures == -1) {
+				Debug.Log ("Texture don't exit");
+			}
 		
 		
-		//hex.renderer.sharedMaterial = Resources.Load("Textures/Terrain/Materials/"+terrain_type) as Material;
-		hex.renderer.material.mainTexture= Resources.Load("Textures/Terrain/"+terrain_type) as Texture2D;
+			//hex.renderer.sharedMaterial = Resources.Load("Textures/Terrain/Materials/"+terrain_type) as Material;
+			hex.renderer.material.mainTexture = Resources.Load ("Textures/Terrain/" + terrain_type) as Texture2D;
 
 			
-	}
-	
-	private bool IsEven (int number)
-	{
-		if (number % 2 == 0) {
-			return true;
-		} else {
-			return false;
 		}
-	}
 	
-	void Awake ()
-	{
+		private bool IsEven (int number)
+		{
+			if (number % 2 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	
+		void Awake ()
+		{
 		
-		ReadXMLMapFile (mapname);
-		/*for (int i=0; i<height; i++){
+			ReadXMLMapFile (mapname);
+			/*for (int i=0; i<height; i++){
 			for (int j=0; j<width; j++){
 				GameObject hex;
 				if (IsEven(j)){
@@ -111,18 +113,18 @@ public class GUIMap : MonoBehaviour
 				
 			}
 		}*/
+		}
+	
+		// Use this for initialization
+		void Start ()
+		{
 		
-	}
+		}
 	
-	// Use this for initialization
-	void Start ()
-	{
-		
-	}
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	// Update is called once per frame
-	void Update ()
-	{
-	
+		}
 	}
 }
