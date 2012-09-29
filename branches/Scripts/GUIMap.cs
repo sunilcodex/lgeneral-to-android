e7a4200c32577157e0adb48385cb3558639e1530
r.ramosfg <r.ramosfg@gmail.com>
2012-09-29 10:02:54 +0000

    Se han eliminado todos el código comentado que no era necesario y se han añadido nuevo espacios de nombres

--- branches/Scripts/GUIMap.cs
+++ branches/Scripts/GUIMap.cs
@@ -1,80 +1,18 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 using System.Xml;
 using System.IO;
 using System.Xml.Serialization;
 using Engine;
+using Miscellaneous;
 
 public class GUIMap : MonoBehaviour
 {
 	
 	public GameObject hexPrefab;
-	//public Material defaultTexture;
 	public string mapname;
-	//private string title;
-	//private string description;
-	
-	//public TipoSuelo[,] mapa;
-	
-	private void ReadXMLMapFile (string name)
-	{
-		XmlDocument xDoc = new XmlDocument ();
-		xDoc.Load ("Assets/Maps/" + name + ".xml");
-		//title = xDoc.GetElementsByTagName("title")[0].InnerText;
-		//print (title);
-		//description = xDoc.GetElementsByTagName("description")[0].InnerText;
-		//print (description);
-		//width = int.Parse (xDoc.GetElementsByTagName ("map_w") [0].InnerText);
-		//height = int.Parse (xDoc.GetElementsByTagName ("map_h") [0].InnerText);
-		/*XmlNodeList terrain = xDoc.GetElementsByTagName("terrain");
-		XmlNodeList cells = ((XmlElement)terrain[0]).GetElementsByTagName("cell"); 
-		foreach (XmlElement node in cells){
-			int y = Convert.ToInt32(node.SelectSingleNode("position_h").InnerText);
-			int x = Convert.ToInt32(node.SelectSingleNode("position_w").InnerText);
-			string terrain_type = node.SelectSingleNode("terrain_type").InnerText;
-			int orientation = Convert.ToInt32(node.SelectSingleNode("orientation").InnerText);
-			MakeMap(x,y,terrain_type,orientation);
-			
-		}
-		*/
-		
-		XmlNodeList terrain = xDoc.GetElementsByTagName ("map");
-		foreach (XmlElement node in terrain) {
-			int y = Convert.ToInt32 (node.SelectSingleNode ("height").InnerText);
-			int x = Convert.ToInt32 (node.SelectSingleNode ("width").InnerText);
-			string terrain_type = node.SelectSingleNode ("celltype").InnerText.ToLower ();
-			int orientation = Convert.ToInt32 (node.SelectSingleNode ("orientation").InnerText);
-			MakeMap (x, y, terrain_type, orientation);
-			
-		}
-		
-	}
-	
-	private void MakeMap (int x, int y, string terrain_type, int orientation)
-	{
-		GameObject hex;
-		if (IsEven (x)) {
-			hex = (GameObject)Instantiate (hexPrefab, new Vector3 (x * 1.5f, 0, -Mathf.Sqrt (3) * y), Quaternion.identity);
-					
-		} else {
-			hex = (GameObject)Instantiate (hexPrefab, new Vector3 (x * 1.5f, 0,
-				-(Mathf.Sqrt (3) * y) - (Mathf.Sqrt (3) / 2)), Quaternion.identity);
-		}
-		hex.transform.parent = this.gameObject.transform;
-		Hexagon hexScript = hex.GetComponent (typeof(Hexagon)) as  Hexagon;
-		hexScript.actualTexture = orientation;
-		hexScript.maxTextures = TextureTable.GetMaxTextureOf (terrain_type);
-		if (hexScript.maxTextures == -1) {
-			Debug.Log ("Texture don't exit");
-		}
-		
-		
-		//hex.renderer.sharedMaterial = Resources.Load("Textures/Terrain/Materials/"+terrain_type) as Material;
-		hex.renderer.material.mainTexture = Resources.Load ("Textures/Terrain/" + terrain_type) as Texture2D;
-
-			
-	}
 	
 	private bool IsEven (int number)
 	{
@@ -101,8 +39,8 @@ public class GUIMap : MonoBehaviour
 				Hexagon hexScript = hex.GetComponent(typeof(Hexagon)) as  Hexagon;
 				hexScript.actualTexture = map.map[j,i].strat_image_offset;
 				hexScript.maxTextures = TextureTable.GetMaxTextureOf (map.map[j,i].terrain.name.ToLower());
-				SDL_Surface sdl = SDL_Surface.LoadSurface(map.map[j,i].terrain.name.ToLower(),false);
-				hex.renderer.material.mainTexture = sdl.bitmap;
+				SDL_Surface sdl = SDL_Surface.LoadSurface("Textures/terrain/"+map.map[j,i].terrain.name.ToLower(),false);
+				hex.renderer.sharedMaterial = sdl.bitmapMaterial;
 			}
 		}
 	}
@@ -120,27 +58,6 @@ public class GUIMap : MonoBehaviour
 		// Cleanup
 		ReadFileStream.Close ();
 		MakeMap(LoadedObj);
-		//ReadXMLMapFile (mapname);
-		/*for (int i=0; i<height; i++){
-			for (int j=0; j<width; j++){
-				GameObject hex;
-				if (IsEven(j)){
-					hex = (GameObject)Instantiate(hexPrefab,new Vector3(j*1.5f,0,-Mathf.Sqrt(3)*i),Quaternion.identity);
-					
-				}
-				else{
-					hex = (GameObject)Instantiate(hexPrefab,new Vector3(j*1.5f,0,
-						-(Mathf.Sqrt(3)*i)-(Mathf.Sqrt(3)/2)),Quaternion.identity);
-				}
-				hex.transform.parent = this.gameObject.transform;
-				Hexagon hexScript = hex.GetComponent(typeof(Hexagon)) as  Hexagon;
-				hexScript.actualTexture = 1;
-				hexScript.maxTextures = 3;
-				//Material hexMaterial  = hex.renderer;
-				hex.renderer.sharedMaterial = defaultTexture;
-				
-			}
-		}*/
 	}
 	
 	// Use this for initialization
