724e383342070fd2b0620bca2ba7b6a105aea71d
r.ramosfg@gmail.com <r.ramosfg@gmail.com@7aea5e75-5191-deb6-7d89-6dce49e5dd89>
2012-10-03 16:14:38 +0000

    Clase que contiene la posición de una nación dentro del mapa así como que tipo de nación es.

---
+++ branches/Scripts/Flag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Engine
+{
+    [Serializable]
+    public class Flag
+    {
+        public int x, y;
+        public int obj;
+        public string name;
+    }
+}
