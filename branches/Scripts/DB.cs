76318fb06bdc417a929618efaf69a24b479ffbee
r.ramosfg <r.ramosfg@gmail.com>
2012-10-14 21:56:42 +0000

    Clase que actúa como base de datos, centraliza todos las instancias de los objetos que actúan como Base de datos en la aplicación

---
+++ branches/Scripts/DB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Engine;
+
+namespace DataFile
+{
+    public static class DB
+    {
+        public static Unit_Lib_Entry UnitLib = new Unit_Lib_Entry();
+        public static Terrain terrain = new Terrain();
+        public static Setup setup = new Setup();
+        public static STATUS status = new STATUS();                    /* statuses defined in engine_tools.h */
+        public static Scenario scen = new Scenario();
+#if TODO_RR
+        public static campaign camp = new campaign();
+#endif
+        public static Map map = new Map();
+    }
+}
