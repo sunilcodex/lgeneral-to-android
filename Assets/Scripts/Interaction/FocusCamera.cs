696850835865ab32990eb190b5dd8ed390ebc0ce
r.ramosfg@gmail.com <r.ramosfg@gmail.com@7aea5e75-5191-deb6-7d89-6dce49e5dd89>
2013-01-22 19:44:16 +0000

    Centrado de la cámara al centro del mapa

--- Assets/Scripts/Interaction/FocusCamera.cs
+++ Assets/Scripts/Interaction/FocusCamera.cs
@@ -4,20 +4,18 @@ using EngineApp;
 using Miscellaneous;
 
 public class FocusCamera : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 		if (Engine.map.isLoaded){
-			int xpos = Engine.map.map_w*Config.hex_x_offset-15;
-			int zpos = Engine.map.map_h*-Config.hex_h;
-			print (xpos);
-			print (zpos);
-			print(Camera.mainCamera.transform.position.x);
+			float xpos = Engine.map.map_w*Config.hex_x_offset-15;
+			float zpos = Engine.map.map_h*-Config.hex_h+Config.hex_y_offset;
+			Camera.mainCamera.transform.position = new Vector3((xpos/2),50,(zpos/2));
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
