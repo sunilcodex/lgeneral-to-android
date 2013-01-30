c1b1ba71cf2ac9a70bcca296052c7d852bd77d9f
r.ramosfg@gmail.com <r.ramosfg@gmail.com@7aea5e75-5191-deb6-7d89-6dce49e5dd89>
2013-01-30 09:35:47 +0000

    Añadido el método de repintado, con una serie de cambios también a la hora de creación como el cambio de estado y el añadido de los gameobject a un array

--- Assets/Scripts/GUI/GUIMap.cs
+++ Assets/Scripts/GUI/GUIMap.cs
@@ -8,15 +8,14 @@ using DataFile;
 
 public class GUIMap : MonoBehaviour
 {
-	
+        
 	public GameObject hexPrefab;
 	public string scen_name;
-	
-	
-	
-	
+	private GameObject[,] mapped;
+        
 	private void MakeMap (Map map)
 	{
+		mapped = new GameObject[map.map_w,map.map_h];
 		//Create Map
 		for (int i=0; i<map.map_h; i++) {
 			for (int j=0; j<map.map_w; j++) {
@@ -24,38 +23,73 @@ public class GUIMap : MonoBehaviour
 				if (Misc.IsEven (j)) {
 					//hex = (GameObject)Instantiate (hexPrefab, new Vector3 (j * 1.5f, 0, -Mathf.Sqrt (3) * i), Quaternion.identity);
 					hex = (GameObject)Instantiate (hexPrefab, new Vector3 (j * Config.hex_x_offset, 0, 
-													-Config.hex_h * i), Quaternion.identity);
+                                                                                                        -Config.hex_h * i), Quaternion.identity);
 				} else {
-					//hex = (GameObject)Instantiate (hexPrefab, new Vector3 (j * 1.5f, 0,		
+					//hex = (GameObject)Instantiate (hexPrefab, new Vector3 (j * 1.5f, 0,           
 					//-(Mathf.Sqrt (3) * i) - (Mathf.Sqrt (3) / 2)), Quaternion.identity);
 					hex = (GameObject)Instantiate (hexPrefab, new Vector3 (j * Config.hex_x_offset, 0,
-													-(Config.hex_h * i) - Config.hex_y_offset), Quaternion.identity);
+                                                                                                        -(Config.hex_h * i) - Config.hex_y_offset), Quaternion.identity);
 				}
+				mapped[j,i] = hex;
 				//put hex as child of map
 				hex.transform.parent = this.gameObject.transform;
 				//TODO_RR AddTextureTerrain (hex, map.map [j, i]);
 				SDL_Surface hexTex;
 				//Draw Terrain
-				hexTex = map.map_draw_terrain(j,i);
-				if (map.map [j, i].g_unit!=null || map.map [j, i].a_unit!=null){
-						hexTex = map.map_draw_units(hexTex,j,i,!Engine.Air_mode,false);					
+				hexTex = map.map_draw_terrain (j, i);
+				if (map.map [j, i].g_unit != null || map.map [j, i].a_unit != null) {
+					hexTex = map.map_draw_units (hexTex, j, i, !Engine.Air_mode, false);                                 
 				}
 				hex.renderer.material = hexTex.BitmapMaterial;
-				
+                                
 			}
 		}
+		Engine.status = STATUS.STATUS_NONE;
 	}
-
-	
-	private void onLoadScen(){
-		Engine.engine_set_status(STATUS.STATUS_NONE);
-		Engine.engine_init(scen_name);
-		Engine.engine_run();
-		Engine.engine_begin_turn(Engine.cur_player, DB.setup.type == SETUP.SETUP_LOAD_GAME);
+    
+	void Repaint(Map map, bool use_frame){
+		if (Engine.draw_map) {
+			print (Engine.status);
+			print ("Hay que repintar");
+			//Comprobar las nieblas
+			for (int i=0; i<map.map_h; i++) {
+                for (int j=0; j<map.map_w; j++) {
+					SDL_Surface hexTex;
+					//Draw Terrain
+					hexTex = map.map_draw_terrain (j, i);
+					if (map.map [j, i].g_unit != null || map.map [j, i].a_unit != null) {
+						 if (Engine.cur_unit != null && Engine.cur_unit.x == j && Engine.cur_unit.y == i
+							&& Engine.status != STATUS.STATUS_MOVE && map.mask[j, i].spot){
+							hexTex = map.map_draw_units (hexTex, j, i, !Engine.Air_mode, use_frame); 
+						}
+						else{
+							hexTex = map.map_draw_units (hexTex, j, i, !Engine.Air_mode, false);
+						}
+					}
+					mapped[j,i].renderer.material = hexTex.BitmapMaterial;
+					//mapped[j,i].renderer.material.color = Color.blue;
+					/*if (!Engine.map.mask[j,i].fog){
+						print (i+","+j);
+					}*/
+				}
+			}
+					
+#if TODO_RR
+       añadir las nieblas y repintar
+#endif
+			Engine.draw_map = false;
+		}
+	}
+	private void onLoadScen ()
+	{
+		Engine.engine_set_status (STATUS.STATUS_NONE);
+		Engine.engine_init (scen_name);
+		Engine.engine_run ();
+		Engine.engine_begin_turn (Engine.cur_player, DB.setup.type == SETUP.SETUP_LOAD_GAME);
 		//Scenario.scen_load (scen_name);
 		MakeMap (Engine.map);
 	}
-	
+        
 	void Awake ()
 	{
 		if (string.IsNullOrEmpty (scen_name)) {
@@ -64,25 +98,19 @@ public class GUIMap : MonoBehaviour
 		//onLoadScen();
 		//Scenario.scen_load("Poland.xml");
 		MakeMap (Engine.map);
-				
+                                
 	}
-	
+        
 	// Use this for initialization
 	void Start ()
 	{
-		
+                
 	}
-	
+        
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Engine.draw_map){
-			print (Engine.status);
-			print ("Hay que repintar");
-#if TODO_RR
-			añadir las nieblas y repintar
-#endif
-		 	Engine.draw_map = false;
-		}
+		bool use_frame = (Engine.cur_ctrl != PLAYERCONTROL.PLAYER_CTRL_CPU);
+		Repaint (Engine.map,use_frame);
 	}
 }
