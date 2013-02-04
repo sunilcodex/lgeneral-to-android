50cdbace67b6139301ef22c97b35107af2a1b008
r.ramosfg <r.ramosfg@gmail.com>
2013-02-04 08:25:21 +0000

    Cambiado el mensaje de lo que se muestra en la GUI, así como su tamaño.

--- Assets/Scripts/GUI/GUIScenInfo.cs
+++ Assets/Scripts/GUI/GUIScenInfo.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 using System.Collections;
 using Miscellaneous;
+using EngineApp;
 
 [ExecuteInEditMode]
 public class GUIScenInfo : MonoBehaviour {
 	
 	void OnGUI ()
 	{
-		GUI.Window (0, new Rect ((Screen.width / 2) - 150, (Screen.height / 2)-100, 300, 200), ShowInfo, "");
+		GUI.Window (0, new Rect ((Screen.width / 2) - 150, (Screen.height / 2)-150, 300, 300), ShowInfo, "");
 	}
 	
 	void ShowInfo (int windowID)
 	{
-		GUI.Label(new Rect(68,50,200,75),Config.Show_info_scen);
-		if (GUI.Button(new Rect(125,125,50,25),"OK")){
+		string info = "";
+		if (Engine.map.isLoaded){
+			info = Engine.GuiShowScenInfo();
+		}
+		GUI.Label(new Rect(20,30,300,400),info);
+		if (GUI.Button(new Rect(125,235,50,25),"OK")){
 			Application.LoadLevel("Map");
 		}
 	}
