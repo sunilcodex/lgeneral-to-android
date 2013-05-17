b815b3cf73b85d0548345dd8cab2c96b6e7c876f
r.ramosfg <r.ramosfg@gmail.com>
2013-05-17 15:20:33 +0000

    Cambiado el método WindowMenu para da lugar al nuevo formato de GUI, también se han añadido o cambiado los atributos necesarios para llevar a cabo esto.

--- Assets/Scripts/GUI/GUIMenu.cs
+++ Assets/Scripts/GUI/GUIMenu.cs
@@ -5,68 +5,76 @@ using EngineApp;
 using DataFile;
 
 [ExecuteInEditMode]
-	public class GUIMenu : MonoBehaviour
+public class GUIMenu : MonoBehaviour
 {
 	
 	private int selGridInt = 0;
 	public GUIStyle myStyle;
 	public GUIStyle myStyle2;
-	private string[] selOption = new string[] {"Campaign", "Config"};
+	private string[] selOption = new string[] {"Campaign", "Config", "About"};
+
+	void Awake ()
+	{
+		Misc.set_random_seed ();
+	}
 	
 	void OnGUI ()
 	{
-		GUI.Window (0, new Rect ((Screen.width / 2) - 200, (Screen.height / 2)-175, 400, 350), WindowMenu, "");
+		GUI.Window (0, new Rect ((Screen.width / 2) - 200, (Screen.height / 2) - 175, 400, 350), WindowMenu, "");
 	}
 	
-	void Start () {
-		Misc.set_random_seed();
-	}
-	
-	int rButtonWithGrid ()
+	private int rButtonWithGrid ()
 	{
 		
-		return GUILayout.SelectionGrid (selGridInt, selOption, 2); 
+		return GUILayout.SelectionGrid (selGridInt, selOption, 3); 
 			
 	}
 	
 	void WindowMenu (int windowID)
 	{
-		GUI.Label(new Rect(70,40,80,25),"Imagen",myStyle2);
-		GUI.Label(new Rect(200,40,100,25),"LGeneral",myStyle);
-		GUILayout.Space (70);
-		selGridInt = rButtonWithGrid ();
-		GUI.Box (new Rect (10, 120, 380, 180), "");	
-		switch (selGridInt) {
-		case 0:
-			if (GUI.Button(new Rect(50, 140, 100, 20), new GUIContent("1939", "Germany launches a series of blizkrieg attacks in Europe, beggining with Poland"))){
-				//Config.CampaingSelected = "Poland.xml";
-				OnLoadCampaign("Poland.xml");
+			GUI.Label (new Rect (70, 40, 80, 25), "Imagen", myStyle2);
+			GUI.Label (new Rect (200, 40, 100, 25), "LGeneral", myStyle);
+			GUILayout.Space (70);
+			selGridInt = rButtonWithGrid ();
+			GUI.Box (new Rect (10, 120, 380, 180), "");
+			switch (selGridInt) {
+			case 0:
+				if (GUI.Button (new Rect (50, 140, 100, 20), new GUIContent ("1939", "Germany launches a series of blizkrieg attacks in Europe, beggining with Poland"))) {
+					Config.CampaignSelected = "Poland.xml";
+					Application.LoadLevel("ScenInfo");
+				}
+				if (GUI.Button (new Rect (50, 170, 100, 20), new GUIContent ("1941 West", 
+				"Hungry for the oil of the Middle East, the Axis moves through North Africa")))
+					Application.LoadLevel ("CuboPrueba");
+				if (GUI.Button (new Rect (50, 200, 100, 20), 
+				new GUIContent ("1941 East", "In the greatest invasion of history, the Axis strikes into the Soviet Union")))
+					Application.LoadLevel ("CuboPrueba");
+				if (GUI.Button (new Rect (50, 230, 100, 20), 
+				new GUIContent ("1943 West", "The Axis must defend the soft underbelly of Europe from the Allies")))
+					Application.LoadLevel ("CuboPrueba");
+				if (GUI.Button (new Rect (50, 260, 100, 20), new GUIContent ("1943 East", 
+				"Overextended Soviet forces, the Axis tries to regain the initiative in the East")))
+					Application.LoadLevel ("CuboPrueba");
+				GUI.Label (new Rect (200, 175, 150, 100), GUI.tooltip);
+				break;
+			case 1:
+				Config.supply = GUI.Toggle (new Rect (145, 155, 200, 30), Config.supply, "Units must supply?");
+				Config.weather = GUI.Toggle (new Rect (145, 185, 200, 30), Config.weather, "Does weather have?");
+				Config.fog_of_war = GUI.Toggle (new Rect (145, 215, 200, 30), Config.fog_of_war, "Fog of war?");
+				Config.show_cpu_turn = GUI.Toggle (new Rect (145, 245, 200, 30), Config.show_cpu_turn, "Show CPU turn?");
+				break;
+			case 2:
+				GUI.Label(new Rect(50,130,100,20),"Version: 1.0");
+				GUI.Label(new Rect(50,150,200,20),"Autor: Ruben Ramos");
+				GUI.Label(new Rect(50,170,300,120),"This code is based on the adaptation of a project " +
+					"based on the game LGeneral Copyright 2001-2005 by Michael Speck. This program is free software; " +
+					"you can redistribute it and/or modify it under the terms of the GNU General Public License as " +
+					"published by the Free Software Foundation.");
+				break;
 			}
-			GUI.Button(new Rect(50, 170, 100, 20), new GUIContent("1941 West", "Hungry for the oil of the Middle East, the Axis moves through North Africa"));
-			GUI.Button(new Rect(50, 200, 100, 20), new GUIContent("1941 East", "In the greatest invasion of history, the Axis strikes into the Soviet Union"));
-			GUI.Button(new Rect(50, 230, 100, 20), new GUIContent("1943 West", "The Axis must defend the soft underbelly of Europe from the Allies"));
-			GUI.Button(new Rect(50, 260, 100, 20), new GUIContent("1943 East", "Overextended Soviet forces, the Axis tries to regain the initiative in the East"));
-			GUI.Label(new Rect(200, 175, 150, 100), GUI.tooltip);
-			break;
-		case 1:
-			Config.supply = GUI.Toggle(new Rect(145,155,200,30),Config.supply,"Units must supply?");
-			Config.weather = GUI.Toggle(new Rect(145,185,200,30),Config.weather,"Does weather have?");
-			Config.fog_of_war = GUI.Toggle(new Rect(145,215,200,30),Config.fog_of_war,"Fog of war?");
-			Config.show_cpu_turn = GUI.Toggle(new Rect(145,245,200,30),Config.show_cpu_turn,"Show CPU turn?");
-			break;
-		}
-		if (GUI.Button(new Rect(170,310,55,25),"Exit")){
-			Application.Quit ();
-		}		
-        
+			if (GUI.Button (new Rect (170, 310, 55, 25), "Exit")) {
+				Application.Quit ();
+			}	
 	}
 	
-	private void OnLoadCampaign(string scen_fname){
-		Engine.engine_set_status(STATUS.STATUS_NONE);
-		Engine.engine_init(scen_fname);
-		Engine.engine_run();
-		Config.Show_info_scen= Engine.engine_begin_turn(Engine.cur_player, DB.setup.type == SETUP.SETUP_LOAD_GAME);
-		Application.LoadLevel("GUIScenInfo");
-		
-	}
 }
