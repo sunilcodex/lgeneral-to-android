93e1388fb639aa33b33c026d58072302d5e0ba8c
r.ramosfg@gmail.com <r.ramosfg@gmail.com@7aea5e75-5191-deb6-7d89-6dce49e5dd89>
2013-05-17 15:21:35 +0000

    Añadido el método Awake el cual hace la carga de todo el sistema, también se ha añadido el atributo info.

--- Assets/Scripts/GUI/GUIScenInfo.cs
+++ Assets/Scripts/GUI/GUIScenInfo.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using System.Collections;
 using Miscellaneous;
 using EngineApp;
+using DataFile;
 
 [ExecuteInEditMode]
 public class GUIScenInfo : MonoBehaviour {
-	
+	private string info;
+
 	void OnGUI ()
 	{
 		GUI.Window (0, new Rect ((Screen.width / 2) - 150, (Screen.height / 2)-150, 300, 300), ShowInfo, "");
@@ -13,22 +15,19 @@ public class GUIScenInfo : MonoBehaviour {
 	
 	void ShowInfo (int windowID)
 	{
-		string info = "";
-		if (Engine.map.isLoaded){
-			info = Engine.GuiShowScenInfo();
-		}
 		GUI.Label(new Rect(20,30,300,400),info);
 		if (GUI.Button(new Rect(125,235,50,25),"OK")){
 			Application.LoadLevel("Map");
 		}
 	}
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Awake(){
+		Engine.engine_set_status(STATUS.STATUS_NONE);
+		Engine.engine_init(Config.CampaignSelected);
+		Engine.engine_run();
+		Engine.engine_begin_turn(Engine.cur_player, DB.setup.type == SETUP.SETUP_LOAD_GAME);
+		if (Engine.map.isLoaded){
+			info = Engine.GuiShowScenInfo();
+		}
 	}
 }
